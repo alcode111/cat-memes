e370769ce559ee63c08a41497cc56c47cde4c587
Ismail <141173099+alcode111@users.noreply.github.com>
2024-02-19 22:32:55 +0100

    Implement a getMatchingCatsArray function to retrieve an array of objects based on the user input

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import { catsData } from '/data.js'
 
 const emotionRadios = document.getElementById('emotion-radios')
+const getImageBtn = document.getElementById('get-image-btn')
+const gifsOnlyOption = document.getElementById('gifs-only-option')
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
+getImageBtn.addEventListener('click', getMatchingCatsArray)
+
+function getMatchingCatsArray() {
+    const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+    const isGif = gifsOnlyOption.checked
+
+    const matchingCatsArray = 
+    catsData.filter(cat => isGif ? 
+        cat.emotionTags.includes(selectedEmotion) && cat.isGif : 
+        cat.emotionTags.includes(selectedEmotion))
+    
+    return matchingCatsArray
+}
 
 function highlightCheckedOption(e) {
     const radios = document.getElementsByClassName('radio')
