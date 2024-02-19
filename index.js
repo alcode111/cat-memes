84051bc7cf8edd395c56de2990247b3bbf2d98cf
Ismail <141173099+alcode111@users.noreply.github.com>
2024-02-19 20:31:34 +0100

    Implement logic to improve getEmotionsArray and remove duplicates

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ function getEmotionsArray(cats) {
 
     for (let cat of cats) {
         for (let emotion of cat.emotionTags) {
-            emotionsArray.push(emotion)
+            if (!emotionsArray.includes(emotion)) {
+                emotionsArray.push(emotion)
+            }
         }
     }
     return emotionsArray
